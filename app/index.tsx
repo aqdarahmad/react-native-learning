1eac3ad4c5f89a3bb6629f76fc3ff33581676a98
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-02-06 00:43:05 +0200

    Aqdar (#2)
    * hellow world
    
    * simple style

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,24 @@
-import {View} from 'react-native'
+import {View , Text , StyleSheet} from 'react-native'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
 
 export default function App(){
   return (
-   <View>
-    Welcome qween Aqdar
+   <View style={styles.container}>
+    <Text style={styles.text}>
+        Welcome qween Aqdar
+    </Text>
+   
    </View>
 
   )
